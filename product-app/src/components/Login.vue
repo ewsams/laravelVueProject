65ab5f336ed38334ae7604b17c382e4cdedfbbaf
ewsams <edwardsams@email.arizona.edu>
2021-07-11 19:57:15 -0700

    request made logo

--- product-app/src/components/Login.vue
+++ product-app/src/components/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-3">
-    <h3>Returning?</h3>
+    <h3>Returning? Log In...</h3>
     <form>
-      <input class="m-2" type="text" placeholder="email" />
-      <input class="m-2" type="text" placeholder="password" />
+      <input class="m-2" size="30" type="text" placeholder="email" />
+      <input class="m-2" size="30" type="text" placeholder="password" />
       <button class="btn success-btn" type="submit">Log In</button>
     </form>
   </div>
--- product-app/src/store/modules/user.js
+++ product-app/src/store/modules/user.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const state = {
     user: null,
+    token:null,
     loggedIn: false
 };
 
@@ -13,19 +14,28 @@ const actions = {
         const response = await axios.post('http://localhost:8000/api/register',
             name, email, password);
         console.log(response);
-        commit('SET_USER', response);
+        commit('SET_USER', response.data.user);
+        commit('SET_TOKEN',response.data.token);
     },
     LOGIN: async ({ commit }, email, password) => {
         const response = await axios.post('http://localhost:8000/api/login',
             email, password);
         console.log(response.data);
-        commit('SET_USER', response.data);
+        commit('SET_USER', response.data.user);
+        commit('SET_TOKEN',response.data.token);
+        commit('SET_LOGIN_STATUS',true);
     }
 };
 
 const mutations = {
    SET_USER: (state, user) => {
         state.user = user;
+    },
+    SET_TOKEN:(state,token) => {
+        state.token = token;
+    },
+    SET_LOGIN_STATUS:(state,login) => {
+        state.loggedIn = login;
     }
 };
 
