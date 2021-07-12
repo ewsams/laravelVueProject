8710d68f77b3e97605834ebe561f96be8da5f095
ewsams <edwardsams@email.arizona.edu>
2021-07-11 18:29:59 -0700

    request made logo

--- product-app/src/views/Home.vue
+++ product-app/src/views/Home.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <Login/>
-    </div>
+    <img
+      alt="Laravel logo"
+      style="height: 10rem; width: 10rem"
+      src="../assets/laravel.png"
+      class="m-3"
+    />
+    <Login />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Login from '@/components/Login';
+import Login from "@/components/Login";
 
 export default {
   name: "Home",
   components: {
-    Login
+    Login,
   },
-  methods:{
-    async login(){
-
-    }
-  }
+  methods: {
+    async login() {},
+  },
 };
 </script>
 
