1983d78354f183d14365cc1d4ae590b6c7141745
ewsams <edwardsams@email.arizona.edu>
2021-07-13 14:51:28 -0700

    validation added

--- product-app/src/components/Login.vue
+++ product-app/src/components/Login.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="p-3">
-    <h3>Returning? Log In...</h3>
-    <form @submit.prevent="login">
-      <input
-        class="m-2"
-        size="30"
-        type="text"
-        placeholder="email"
-        v-model="email"
-      />
-      <input
-        class="m-2"
-        size="30"
-        type="text"
-        placeholder="password"
-        v-model="password"
-      />
-      <button class="btn success-btn" type="submit">Log In</button>
-    </form>
+    <div v-if="!loggedInStatus">
+      <h3>Returning? Log In...</h3>
+      <form @submit.prevent="login">
+        <input
+          class="m-2"
+          size="30"
+          type="text"
+          placeholder="email"
+          v-model="email"
+        />
+        <input
+          class="m-2"
+          size="30"
+          type="text"
+          placeholder="password"
+          v-model="password"
+        />
+        <button class="btn success-btn" type="submit">Log In</button>
+      </form>
+    </div>
+    <div v-if="loggedInStatus">
+      <h3>You are currently Logged In...</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "Login",
@@ -31,6 +36,9 @@ export default {
       email: "",
       password: "",
     };
+  },
+  computed: {
+    ...mapGetters({ loggedInStatus: "user/GET_LOGGED_IN_STATUS" }),
   },
   methods: {
     ...mapActions({ userLogin: "user/LOGIN" }),
