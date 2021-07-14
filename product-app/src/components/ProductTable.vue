6ab0d9cea5659ab5f6b582c400c5bbbd8e25e7fc
ewsams <edwardsams@email.arizona.edu>
2021-07-13 18:59:47 -0700

    table refactor

---
+++ product-app/src/components/ProductTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="product in products" :key="product.id">
+      <table class="table p-4">
+        <thead>
+          <tr class="table-primary p-3">
+            <th scope="col">{{ product.name }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ product.description }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import store from "../store/index";
+
+export default {
+  name: "ProductTable",
+  mounted() {
+    store.dispatch("product/RETRIEVE_PRODUCTS");
+  },
+  computed: {
+    ...mapGetters({
+      products: "product/GET_PRODUCTS",
+    }),
+  },
+};
+</script>
+
+<style>
+</style>
--- product-app/src/components/UserPage.vue
+++ product-app/src/components/UserPage.vue
@@ -2,36 +2,23 @@
   <div>
     <h4>Welcome back {{ currentUser.name.toUpperCase() }}</h4>
     <h5>Current Available Products</h5>
-    <div v-for="product in products" :key="product.id">
-      <table class="table p-4">
-        <thead>
-          <tr class="table-primary p-3">
-            <th scope="col">{{ product.name }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ product.description }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <ProductTable />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import ProductTable from "../components/ProductTable.vue";
+import { mapGetters} from "vuex";
 import store from "../store/index";
 
 export default {
   name: "UserPage",
-  mounted() {
-    store.dispatch("product/RETRIEVE_PRODUCTS");
+  components: {
+    ProductTable,
   },
   computed: {
     ...mapGetters({
       currentUser: "user/GET_USER",
-      products: "product/GET_PRODUCTS",
     }),
   },
 };
